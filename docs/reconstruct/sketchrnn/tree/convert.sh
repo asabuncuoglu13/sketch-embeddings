e120b4325bab67c350e6a9860f340b3ad4f3d93f
alpay sabuncuoglu <asabuncuoglu13@ku.edu.tr>
2021-03-24 21:31:02 +0300

    reconstructed-images-28

---
+++ docs/reconstruct/sketchrnn/tree/convert.sh
@@ -0,0 +1,13 @@
+convert -resize 28x28 1.png 1.jpg
+convert -resize 28x28 2.png 2.jpg
+convert -resize 28x28 3.png 3.jpg
+convert -resize 28x28 4.png 4.jpg
+convert -resize 28x28 5.png 5.jpg
+#convert -resize 28x28 6.png 6.jpg
+
+rm 1.png
+rm 2.png
+rm 3.png
+rm 4.png
+rm 5.png
+#rm 6.png
